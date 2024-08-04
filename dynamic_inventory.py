e3beb06ececbfb93c669a915d52bf3a2e9e5f10d
Komal Jaiswal <99614372+komalthakur853@users.noreply.github.com>
2024-08-04 15:20:01 +0530

    Update dynamic_inventory.py

--- dynamic_inventory.py
+++ dynamic_inventory.py
@@ -5,7 +5,7 @@ inventory = {
     "all": {
         "hosts": {
             "debian_host": {
-                "ansible_host": "54.159.57.134",
+                "ansible_host": "10.0.10.161",
                 "ansible_ssh_private_key_file": "/home/ubuntu/aws.pem",
                 "ansible_user": "ubuntu"
             }
