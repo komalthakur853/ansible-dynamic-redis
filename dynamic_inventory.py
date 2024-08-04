84b61f3e867c70f0a657e405187a3ff66f48b1e9
Komal Jaiswal <99614372+komalthakur853@users.noreply.github.com>
2024-08-04 14:50:39 +0530

    Update dynamic_inventory.py

--- dynamic_inventory.py
+++ dynamic_inventory.py
@@ -3,16 +3,11 @@ import json
 
 inventory = {
     "all": {
-        "hosts": ["debian_host"],
-        "vars": {
-            "ansible_user": "ubuntu"
-        }
-    },
-    "_meta": {
-        "hostvars": {
+        "hosts": {
             "debian_host": {
                 "ansible_host": "54.91.199.248",
-                "ansible_ssh_private_key_file": "/home/ubuntu/aws.pem"
+                "ansible_ssh_private_key_file": "/home/ubuntu/aws.pem",
+                "ansible_user": "ubuntu"
             }
         }
     }
